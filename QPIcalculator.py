1c10e918f63208e3482421578150e09f807e7fd1
GDivino <gio.divino3649@gmail.com>
2020-09-30 19:06:33 +0800

    simple edit

--- QPIcalculator.py
+++ QPIcalculator.py
@@ -1,5 +1,7 @@
 gradeequivalents = {"A": str(4), "B+": str(3.5), "B": str(3), "C+": str(2.5), "C": str(2), "D": str(1.5), "F": str(1)}
 print(gradeequivalents)
+print()
+print("type x in \"Grade\" to exit the code!")
 
 QPI = 0
 units = 0
@@ -9,28 +11,32 @@ qpiequivalent = 0
 leave = "n"
 
 print("")
-while leave.lower() == "n":
-    grade = str(input("Grade: "))
-    if grade.upper() == "A":
+while leave.upper() == "N":
+    grade = str(input("Grade: ")).upper()
+    if grade == "A":
         qpiequivalent = 4
-    elif grade.upper() == "B+":
+    elif grade == "B+":
         qpiequivalent = float(3.5)
-    elif grade.upper() == "B":
+    elif grade == "B":
         qpiequivalent = 3
-    elif grade.upper() == "C+":
+    elif grade == "C+":
         qpiequivalent = float(2.5)
-    elif grade.upper() == "C":
+    elif grade == "C":
         qpiequivalent = 2
-    elif grade.upper() == "D":
+    elif grade == "D":
         qpiequivalent = float(1.5)
-    elif grade.upper() == "F":
+    elif grade == "F":
         qpiequivalent = 0
+    elif grade == "X":
+        print("Final qpi = {:.2f}".format(QPI))
+        break
     else:
         print("Invalid input")
         break
 
+
     units = int(input("Units: "))
-    if units == str:
+    if int(units) == str:
         print("Invalid input")
         break
     else:
@@ -38,13 +44,5 @@ while leave.lower() == "n":
         units1 += units
         sum2 += sum1
         QPI = sum2 / units1
-        print("QPI: " + str(QPI))
+        print("QPI: {:.2f}".format(QPI))
         print("")
-
-    leave = input("are you done? ")
-    if leave.upper() == "y":
-        print("Final QPI: " + str(qpiequivalent))
-    else:
-        continue
-
-print("Final qpi = " + str(QPI))
